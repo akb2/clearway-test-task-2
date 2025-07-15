01b3ab9bfcd113862cb685a213fd8929240050bc
akb2 <akb2-online@ya.ru>
2025-07-16 01:53:01 +0300

    TEST: Базовые методы

---
+++ src/app/helpers/app.ts
@@ -0,0 +1,9 @@
+/**
+ * Проверка переменной на инициализацию
+ * Если значение равно null или undefined, возвращает false.
+ *
+ * @param value Проверяемое значение
+ * @returns Результат проверки TRUE/FALSE
+ * @template D - Тип значения
+ */
+export const IsDefined = <T>(value?: T): boolean => value !== undefined && value !== null;
