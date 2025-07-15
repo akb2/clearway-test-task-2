f0e01319594c3b5fcbc69de1f2e9aea5aed08441
akb2 <akb2-online@ya.ru>
2025-07-15 23:25:27 +0300

    TEST: Alias'ы

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { DocumentsRoutes } from "./pages/documents/documents.routes";
+import { DocumentsRoutes } from "@pages/documents/documents.routes";
 
 export const routes: Routes = [
   // Основное
