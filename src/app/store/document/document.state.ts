2ae15aa6f28947afecd229dd9e7be5a79c44f2ec
akb2 <akb2-online@ya.ru>
2025-07-16 16:09:17 +0300

    TEST: Убрал неиспользуемый ключ

--- src/app/store/document/document.state.ts
+++ src/app/store/document/document.state.ts
@@ -1,7 +1,6 @@
 import { DocumentItem } from "@models/document";
 
 export const DOCUMENT_KEY = "document";
-export const DOCUMENT_SNIPPETS_LOCAL_STORAGE_KEY = "document-snippets";
 
 export interface DocumentState {
   documents: DocumentItem[];
